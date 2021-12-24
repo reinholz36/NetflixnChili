53963db5fa2517c773f2b5586f5b359e4d71ba6a
reinholz36 <dreinholz28@gmail.com>
2021-12-24 13:49:36 -0500

    added api information in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,62 @@
 
-//Testing file links
-// console.log("file is linked correctly");
+//Recipe fetch the 479101 in the url below is the recipe id number
+// API URL: https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
+var getRecipe = function () {
+    
+fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "1bcdcffaf7msh8ee39bd8bb2df59p1dd6e0jsn113cf6355a62"
+	}
+})
+.then(response => {
+	if (response.ok) {
+        response.json().then(function(data) {
+            console.log("Recipe data", data);
+        });
+    } else {
+        //!TO REPLACE: This alert will need to be replaced with a modal
+        alert('Error: Recipe Not Found')
+    }
+})
+.catch(err => {
+	console.error(err);
+});
 
-// window.onload = function() {
-//     if (window.jQuery) {
-//         alert("yeah!");
-//     } else {
-//         alert("doesn't work");
-//     }
-// }
+}
+
+// getRecipe();
+
+
+
+//User enteres movie name and getMovie poster image. Search example Avengers Endgame
+//API URL: https://rapidapi.com/rapidapi/api/movie-database-imdb-alternative/
+var getMovie = function () {
+fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+		"x-rapidapi-key": "1bcdcffaf7msh8ee39bd8bb2df59p1dd6e0jsn113cf6355a62"
+	}
+})
+.then(response => {
+
+    if (response.ok) {
+        response.json().then(function(data) {
+            console.log("Movie data", data);
+            checkIfOnNetflix(data);
+        });
+    } else {
+        //!TO REPLACE: This alert will need to be replaced with a modal
+        alert('Error: Movie Not Found')
+    }
+	
+})
+.catch(err => {
+	console.error(err);
+});
+}
+
+// getMovie();
 
