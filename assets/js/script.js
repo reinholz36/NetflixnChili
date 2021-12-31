d0294b35ff01d8e48ebcbd73ff3be161b8d32a06
Wesley <wabryant97@gmail.com>
2021-12-31 09:07:09 -0500

    search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,88 @@
-console.log("file is linked correctly");
+var formEntry = document.querySelector("#search");
+var submitButton = document.querySelector("#submit-button")
+var searchedMovie = document.querySelector("#searched-movie")
 
-window.onload = function() {
-    if (window.jQuery) {
-        alert("yeah!");
+var formSubmitHandler = function(event) {
+    // prevent page from reloading
+    event.preventDefault();
+
+    var search = formEntry.value.trim();
+
+    if(search) {
+        getRecipe();
+        getMovie();
     } else {
-        alert("doesn't work");
+        alert("Movie not valid")
+        //!TO REPLACE: This alert will need to be replaced with a modal
     }
 }
+
+//Recipe fetch the 479101 in the url below is the recipe id number
+// API URL: https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
+var getRecipe = function () {
+    
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "1bcdcffaf7msh8ee39bd8bb2df59p1dd6e0jsn113cf6355a62"
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log("Recipe data", data);
+            });
+        } else {
+            //!TO REPLACE: This alert will need to be replaced with a modal
+            alert('Error: Recipe Not Found')
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+    
+    // getRecipe();
+    
+    
+    
+//User enteres movie name and getMovie poster image. Search example Avengers Endgame
+//API URL: https://rapidapi.com/rapidapi/api/movie-database-imdb-alternative/
+var getMovie = function () {
+fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+        "x-rapidapi-key": "1bcdcffaf7msh8ee39bd8bb2df59p1dd6e0jsn113cf6355a62"
+    }
+})
+.then(response => {
+
+    //check for movie
+    
+    
+    if (response.ok) {
+        response.json().then(function(data) {
+            console.log("Movie data", data);
+            displayMovieTitle(movieSearched);
+            //checkIfOnNetflix(data);
+        });
+    } else {
+        //!TO REPLACE: This alert will need to be replaced with a modal
+        alert('Error: Movie Not Found')
+    }
+        
+})
+.catch(err => {
+    console.error(err);
+});
+}
+
+var displayMovieTitle = function(movieSearched) {
+    searchedMovie.textContent = movieSearched;
+
+
+};
+
+submitButton.addEventListener("click", formSubmitHandler);
